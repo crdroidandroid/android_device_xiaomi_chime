d7b968155d4b605c8ba1203abe56c6a3299f216d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-03 13:20:23 +0530

    chime: Update dexpreopt configuration
     * strip off as much as the debugging as we can
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- device.mk
+++ device.mk
@@ -319,6 +319,16 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2 \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Dex
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+endif
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \

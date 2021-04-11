646b390b4937beb596dca391d1d1387c35b3248d
Danny Lin <danny@kdrag0n.dev>
2021-04-10 21:52:29 -0700

    juice: Update custom build fingerprint to raven february Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -18,3 +18,9 @@ PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Fingerprint
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"

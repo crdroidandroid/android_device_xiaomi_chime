dec62ad197911a515eb68296e8447d43f5d93974
I-n-o-k <75870644+I-n-o-k@users.noreply.github.com>
2021-05-02 21:25:20 +0700

    juice: Force triple frame buffers

--- product_prop.mk
+++ product_prop.mk
@@ -16,6 +16,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.egl.hw=0 \
     debug.hwui.renderer=opengl \
     debug.sf.latch_unsignaled=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.disable_backpressure=1 \
     debug.sf.hw=0 \
     ro.surface_flinger.use_color_management=true

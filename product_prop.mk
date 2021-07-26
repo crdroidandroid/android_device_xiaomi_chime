09e1f137f48ffda307e4246e510e0fdd36b80629
GtrCraft <nickvanbokhorst@hotmail.com>
2021-07-26 16:17:04 +0200

    juice: Add & update volume steps props
    Change-Id: If90ec151a0736a114b4d3deee4f70e5c03dc857b

--- product_prop.mk
+++ product_prop.mk
@@ -27,8 +27,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.media_vol_steps=15
-
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=11
+    
 # Power-saving props
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.use_data_netmgrd=true \

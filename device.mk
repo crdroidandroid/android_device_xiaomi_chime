c3b8e7a2937891ed751979cb65000444b0584f68
Avinash Dwivedi <kalkioss10@gmail.com>
2022-01-25 21:36:22 +0530

    juice: Add prebuilt GrapheneOS Camera

--- device.mk
+++ device.mk
@@ -71,6 +71,10 @@ PRODUCT_PACKAGES += \
    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
    vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    GrapheneOSCamera
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \

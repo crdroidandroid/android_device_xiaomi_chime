f53dd90f601411c7308bcd97ca425985320b1336
elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
2022-02-12 11:47:21 +0000

    juice: Update custom build fingerprint to raven march
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -28,7 +28,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 S3B1.220218.004 8242181 release-keys"

010dd1bf49cbbab188ff72516e1155aab5b386c3
lynnnnzx <rifqikanekii140202@gmail.com>
2022-02-21 03:05:37 +0700

    juice: Bringup AOSP
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_juice.mk
+    $(LOCAL_DIR)/aosp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_juice-user \
-    arrow_juice-userdebug \
-    arrow_juice-eng
+    aosp_juice-user \
+    aosp_juice-userdebug \
+    aosp_juice-eng
---
+++ aosp_juice.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

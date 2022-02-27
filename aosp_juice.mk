8bcb9040ff8b112421928af71f990b534340cf5b
lynnnnzx <rifqikanekii140202@gmail.com>
2022-02-27 16:20:13 +0000

    juice: Welcome to PixelPlus-UI
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,9 +6,15 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common AOSP stuff.
+# Inherit some common PixelPlus-UI stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := false
+PPUI_MAINTAINER := Lynx
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice

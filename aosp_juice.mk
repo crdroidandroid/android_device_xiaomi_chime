3938ac0d286db02827dcc3a5bf23d3db8e4004e0
lynnnnzx <rifqikanekii140202@gmail.com>
2022-04-06 22:11:22 +0700

    juice: Bump Raven FP to April
    Signed-off-by: Lynx <rifqikanekii140202@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -28,7 +28,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 S3B1.220218.004 8242181 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"

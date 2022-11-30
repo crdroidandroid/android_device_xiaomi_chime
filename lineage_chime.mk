e7a8655d2620fcc64d803c7925e8504ad1c1b9a9
GtrCraft <nickvanbokhorst@hotmail.com>
2022-11-30 09:01:50 +0100

    Revert "chime: Bringup PixelExperience"
    This reverts commit 05be4fa1ab8e02bc46c40064c764042dfb541527.

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -11,14 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_chime
+PRODUCT_NAME := lineage_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 9T

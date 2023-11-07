8281421de9f973dd6158f1076fab9882682f3f9c
itsHanibee <itshanibee@gmail.com>
2023-11-07 10:55:54 +0000

    init: implement a simple logger script to fetch logs easier.
    [dreAd1811] Original author for the whole logger idea.
    [itsHanibee] Made it platform agnostic, the out dir can be changed easily if the builder wishes the logs to be accessable to an unpriviliged user.
    
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- device.mk
+++ device.mk
@@ -424,7 +424,8 @@ PRODUCT_PACKAGES += \
     init.stnfc.rc \
     init.target.rc \
     fstab.qcom \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    logcat.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom

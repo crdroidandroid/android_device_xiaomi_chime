e3a128070c8d87627292d5112bd8375c5be3509d
itsHanibee <itshanibee@gmail.com>
2023-11-14 07:11:13 +0000

    [DNP] init: fixup CPUSets a bit and tune SchedTune's defaults
    These values are for SM6115 (SD622) and are to work in conjunction with hanikrnl.
    
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3829,9 +3829,9 @@ case "$target" in
 
             # cpuset settings
             echo 0-2     > /dev/cpuset/background/cpus
+	    echo 4-7     > /dev/cpuset/camera-daemon/cpus
             echo 0-3     > /dev/cpuset/system-background/cpus
-            echo 4-7     > /dev/cpuset/foreground/boost/cpus
-            echo 0-2,4-7 > /dev/cpuset/foreground/cpus
+            echo 4-6     > /dev/cpuset/foreground/cpus
             echo 0-7     > /dev/cpuset/top-app/cpus
 
             # configure governor settings for little cluster

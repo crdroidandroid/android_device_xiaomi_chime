7e7e2db3bb1f7e8c60051dfd36e227706bd63f65
iusmac <iusico.maxim@libero.it>
2025-07-15 13:21:44 +0000

    chime: config: Update DEX preopt boot image profile location
    They are now moved from {fwb}/config to {fwb}/boot and split into two
    files.
    
    Signed-off-by: Joe7500 <j7545032@gmail.com>

--- device.mk
+++ device.mk
@@ -520,10 +520,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Boot image profiling
-#ifeq ($(TARGET_BUILD_VARIANT),user)
-#PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-#PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-#endif
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
+    frameworks/base/boot/boot-image-profile.txt \
+    frameworks/base/boot/boot-image-profile-extra.txt
+endif
 
 # Telephony
 PRODUCT_PACKAGES += \

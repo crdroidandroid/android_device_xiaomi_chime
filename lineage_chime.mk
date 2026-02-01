c1bab08bdb1931d937631eb6a73a825dcba7a6f1
Joe7500 <j7545032@gmail.com>
2026-02-01 09:26:20 +0200

    chime: comment out TARGET_ENABLE_BLUR
    Shouldn't be default, but leave in as option.
    
    Signed-off-by: Joe7500 <j7545032@gmail.com>

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-TARGET_ENABLE_BLUR := false
+#TARGET_ENABLE_BLUR := false
 
 # Set to true for building a ROM without GAPPS
 # Set to false for building a ROM with GAPPS included
